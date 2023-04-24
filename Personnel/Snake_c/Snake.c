1b9d21e8f447cef89320d56f1dca32fc0347620d
plakine1 <99917057+plakine1@users.noreply.github.com>
2023-04-24 19:43:39 +0200

    Starting snake

--- Personnel/Snake_c/Snake.c
+++ Personnel/Snake_c/Snake.c
@@ -1,1 +1,12 @@
-#include <sdl.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
+// Build command : gcc snake.c -ISDL2\include -LSDL2\lib -Wall -lmingw32 -lSDL2main -lSDL2 -o main
+int main(int argc, char* argv[]){
+    /*Initial i zes the timen, audio, video, joystick,
+    haptic, gamecontrollen and events sutsystems  */
+    SDL_Init(SDL_INIT_EVERYTHING);
+    printf("d");
+    SDL_Quit();
+    return 0;
+    
+}
